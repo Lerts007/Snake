5b39fd76b3c6147182f145966c660a4c4cd26a83
Lerts <oddff@mail.ru>
2020-11-18 21:59:21 +0300

    Создание рамки и пробных точек.

---
+++ Snake/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Отрисовка рамки
+            HorizontalLine Up_line = new HorizontalLine(0, 100, 0, '+');
+            HorizontalLine Down_line = new HorizontalLine(0, 100, 25, '+');
+            VerticalLine Left_line = new VerticalLine(0, 25, 0, '+');
+            VerticalLine Right_line = new VerticalLine(0, 25, 100, '+');
+            Up_line.Draw_F();
+            Down_line.Draw_F();
+            Left_line.Draw_F();
+            Right_line.Draw_F();
+
+            //Отрисовка точки
+            Point p1 = new Point(10, 6, '*');
+            p1.Draw();
+
+
+            Point p2 = new Point(2, 5, '$');
+            p2.Draw();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            plist = new List<Point>();
+            for (int i = yUp; i <= yDown; i++)
+            {
+                Point p = new Point(x, i, sym);
+                plist.Add(p);
+            }
+
+        }
+    }
+}
